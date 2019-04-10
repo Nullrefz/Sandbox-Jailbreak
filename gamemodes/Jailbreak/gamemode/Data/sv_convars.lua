7732495b5a05bf00286b70637465b9de7c52ef0a
David <david.m@euriskomobility.com>
2019-04-10 23:50:18 +0300

    Added Convars

---
+++ gamemodes/Jailbreak/gamemode/Data/sv_convars.lua
@@ -0,0 +1,4 @@
+CreateConVar("jb_max_guards", 5, FCVAR_ARCHIVE, "Max guard limit")
+CreateConVar("jb_guards_ratio", 3, FCVAR_ARCHIVE, "Number of prisoner per guard")
+CreateConVar("jb_min_player", 2, FCVAR_ARCHIVE, "Number of players required to start the game")
+CreateConVar("jb_max_rounds", -1, FCVAR_ARCHIVE, "Number of played rounds need to change the map")
