630e6da8a1478a114863c9a0e6fca9b1a22ff9eb
David <davidmoussa@outlook.com>
2019-06-15 01:33:50 +0300

    Implemented Waypoint Design

--- gamemodes/Jailbreak/gamemode/cl_init.lua
+++ gamemodes/Jailbreak/gamemode/cl_init.lua
@@ -4,20 +4,28 @@ activeCommands = {}
 net.Receive("UpdateCommands", function()
     activeCommands = net.ReadTable()
 end)
-
+local trace = LocalPlayer():GetEyeTrace()
+local angle = trace.HitNormal:Angle()
 hook.Add("PostDrawOpaqueRenderables", "example", function()
-    local trace = LocalPlayer():GetEyeTrace()
-    local angle = trace.HitNormal:Angle()
-    cam.Start3D2D(trace.HitPos, angle + Angle(90, 0, 0), 2)
+
+    cam.Start3D2D(trace.HitPos, angle + Angle(90, 0, 0), math.Clamp(LocalPlayer():GetPos():Distance(trace.HitPos) / 100, 2, 15))
     draw.DrawArc(0, 0, 8.5, 0, 360, 0, Color(255, 255, 255, 10))
     draw.DrawArc(0, 0, 8.5, 8.3, 360, 0, Color(255, 255, 255, 150))
+    draw.DrawArc(0, 0, 6.7, 6.5, 360, 0, Color(255, 255, 255, 150))
 
     for i = 1, 10 do
-        draw.DrawArc(0, 0, 8, 7, math.sin(CurTime()) * 36 / 4 + 36 / 2, 36 * i, Color(255, 255, 255, 150))
+        draw.DrawArc(0, 0, 8, 7, math.sin(CurTime()) * 36 / 8 + 30, 36 * i + CurTime() * 25, Color(255, 255, 255, 150))
     end
 
-    draw.CapsuleBox(0, 0, 4, .2, 360, 45, 2, Color(255, 255, 255, 150))
-    draw.CapsuleBox(0, 0, 4, .2, 360, 45 + 90, 2, Color(255, 255, 255, 150))
-    draw.DrawArc(0, 0, 8.5 + CurTime() * -2 % 5, 8.4 + CurTime() * -2 % 5, 360, 0, Color(255, 255, 255, 2 * CurTime() % 5))
+    draw.ChamferedBox(0, 0, 3, .2, 2, Color(255, 255, 255, 255))
+    draw.ChamferedBox(0, 0, .2, 3, 2, Color(255, 255, 255, 255))
+    draw.DrawArc(0, 0, 8.5 + CurTime() * -4 % 10, 8.4 + CurTime() * -4 % 10, 360, 0, Color(255, 255, 255, 16 * CurTime() % 40))
+    cam.End3D2D()
+    cam.Start3D2D(trace.HitPos, trace.HitNormal:Angle() + Angle(0 ,  (LocalPlayer():GetEyeTrace().HitPos - LocalPlayer():GetEyeTrace().StartPos):Angle().y  , 0), math.Clamp(LocalPlayer():GetPos():Distance(trace.HitPos) / 100, 2, 15))
+    local dist = math.Clamp(LocalPlayer():GetPos():Distance(trace.HitPos) / 100 - 1 , 0, 1)
+    draw.ChamferedBox(0, 0, 21, .1, 2, Color(255, 255, 255, 222 * dist))
+    draw.ChamferedBox(11, 0, 1, 1, 100, Color(255, 255, 255, 222 * dist))
+    draw.DrawArc(0, 0, 6.7, 6.5, 360, 0, Color(255, 255, 255, 150 * dist))
+ 
     cam.End3D2D()
 end)
