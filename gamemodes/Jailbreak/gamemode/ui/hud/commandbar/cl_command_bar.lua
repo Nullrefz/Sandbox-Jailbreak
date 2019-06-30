a1afacdaeaf0f5e8945ffd00093fc814e093d5a5
David <davidmoussa@outlook.com>
2019-06-30 19:29:04 +0300

    Fixed Command Bar

--- gamemodes/Jailbreak/gamemode/ui/hud/commandbar/cl_command_bar.lua
+++ gamemodes/Jailbreak/gamemode/ui/hud/commandbar/cl_command_bar.lua
@@ -1,14 +1,4 @@
 local COMMANDBAR = {}
-local mats = {
-    Material("jailbreak/vgui/afk.png", "smooth"),
-    Material("jailbreak/vgui/crouch.png", "smooth"),
-    Material("jailbreak/vgui/mic.png", "smooth"),
-    Material("jailbreak/vgui/walk.png", "smooth"),
-    Material("jailbreak/vgui/freelook.png", "smooth"),
-    Material("jailbreak/vgui/sprinting.png", "smooth"),
-    Material("jailbreak/vgui/waypoint.png", "smooth"),
-    Material("jailbreak/vgui/jumping.png", "smooth")
-}
 
 function COMMANDBAR:Init()
     self:UpdateInfo()
@@ -16,29 +6,30 @@ function COMMANDBAR:Init()
     self.panel:Dock(FILL)
     local slots = {}
 
-    for i = 1, #mats do
+    for i = 1, #commandType do
         local lerp = 0
         table.insert(slots, lerp)
     end
 
     function self.panel:Paint(width, height)
         local totalWidth = 0
-        for i = 0, #mats -  2 do
+
+        for i = 1, #slots do
             if table.HasValue(activeCommands, i) then
-                slots[i + 1] = math.Clamp(slots[i + 1] + FrameTime() * 10, 0, 1)
+                slots[i] = math.Clamp(slots[i] + FrameTime() * 10, 0, 1)
             else
-                slots[i + 1] = math.Clamp(slots[i + 1] - FrameTime() * 10, 0, 1)
+                slots[i] = math.Clamp(slots[i] - FrameTime() * 10, 0, 1)
             end
 
-            draw.DrawRect(totalWidth , 0, height * slots[i + 1], height, Color(255, 255, 255, 255 * slots[i + 1]), mats[i + 1])
-            totalWidth = totalWidth + (height + 16) * slots[i + 1]
+            draw.DrawRect(totalWidth, 0, height * slots[i], height, Color(255, 255, 255, 255 * slots[i]), Material("jailbreak/vgui/" .. commandType[i] .. ".png", "smooth"))
+            totalWidth = totalWidth + (height + 16) * slots[i]
         end
     end
 end
 
 function COMMANDBAR:UpdateInfo()
-   net.Start("RequestCommands")
-   net.SendToServer()
+    net.Start("RequestCommands")
+    net.SendToServer()
 end
 
 vgui.Register("JailbreakCommandBar", COMMANDBAR)
