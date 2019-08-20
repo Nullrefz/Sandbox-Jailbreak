2bd302d5130e0db02217ce0f58f95e2b4f4c8e6a
David <davidmoussa@outlook.com>
2019-08-20 21:55:22 +0300

    Implemented Feed Panels

--- gamemodes/Jailbreak/gamemode/ui/hud/killfeed/cl_killfeed_bar.lua
+++ gamemodes/Jailbreak/gamemode/ui/hud/killfeed/cl_killfeed_bar.lua
@@ -1,6 +1,22 @@
 local KILLFEEDBAR = {}
 
 function KILLFEEDBAR:Init()
+    self.progress = 0
+    self.victimIcon = vgui.Create("JailbreakPlayerIcon", self)
+    self.murdererIcon = vgui.Create("JailbreakPlayerIcon", self)
+end
+
+function KILLFEEDBAR:Paint(width, height)
+end
+
+function KILLFEEDBAR:StartEntryAnimation()
+end
+
+function KILLFEEDBAR:AssignPlayers(victim, murderer)
+    self.victim = victim
+    self.murderer = murderer
+    self.victimIcon:SetPlayer(self.victim)
+    self.murdererIcon:SetPlayer(self.murderer)
 end
 
 vgui.Register("JailbreakKillFeedBar", KILLFEEDBAR)
--- gamemodes/Jailbreak/gamemode/ui/hud/killfeed/cl_killfeed_panel.lua
+++ gamemodes/Jailbreak/gamemode/ui/hud/killfeed/cl_killfeed_panel.lua
@@ -1,6 +1,32 @@
 local KILLFEEDPANEL = {}
+local totalHeight = 0
 
 function KILLFEEDPANEL:Init()
+    self.noticePool = {}
+
+    net.Receive("PlayerDied", function()
+        self:AddDeathNotice(player.GetBySteamID(net.ReadString()), player.GetBySteamID64(net.ReadString()))
+    end)
+end
+
+function VOICEPANEL:Think(width, height)
+    self:SetTall(Lerp(FrameTime() * 20, self:GetTall(), totalHeight))
+
+    --    if not table.IsEmpty(self.voiceBars) then return end
+    for i = 1, #self.voiceBars do
+        if IsValid(v) then
+            v:SetPos(self:GetWide() - v:GetWide(), i * v:GetTall())
+        end
+    end
+end
+
+function VOICEPANEL:AddDeathNotice(victim, killer)
+    local deathNotice = vgui.Create("JailbreakKillFeedBar", self)
+    table.insert(self.noticePool, deathNotice)
+    deathNotice:AssignPlayers(victim, killer)
+    deathNotice:SetSize(self:GetWide(), 45)
+    deathNotice:StartEntryAnimation()
+    totalHeight = totalHeight + v:GetTall() * #self.noticePool
 end
 
 vgui.Register("JailbreakKillFeedPanel", KILLFEEDPANEL)
