0472c7986a48af41ac81f333a79cec38397b0cff
David <davidmoussa@outlook.com>
2020-04-05 11:23:01 +0300

    Implemented Log Index

---
+++ gamemodes/Jailbreak/gamemode/Logs/panel/cl_log_index.lua
@@ -0,0 +1,37 @@
+local LOGINDEX = {}
+
+function LOGINDEX:Init()
+    local alpha = 0
+    self.player = nil
+    self.panel = vgui.Create("DButton", self)
+    self.panel:SetText("")
+    self.index = 0
+    LerpFloat(0, 1, 1, function(progress)
+        if not alpha then return end
+        alpha = progress
+    end, INTERPOLATION.SinLerp)
+
+    function self.panel:Paint(width, height)
+        draw.DrawRect(0, 0, width, height, Color(30, 30, 30, alpha * 255))
+
+        if self:IsHovered() then
+        draw.DrawRect(0, 0, width, height, Color(255, 255, 255, alpha * 30))
+
+        end
+    end
+
+    self.panel.DoClick = function()
+        hook.Run("LogClicked", self.player:SteamID(), self.index)
+    end
+end
+
+function LOGINDEX:PerformLayout(width, height)
+    self.panel:Dock(FILL)
+end
+
+function LOGINDEX:SetInfo(player, index)
+    self.player = player
+    self.index = index
+end
+
+vgui.Register("JailbreakLogIndex", LOGINDEX)
