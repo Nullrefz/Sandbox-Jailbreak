60bcb607ffae99e0c0ed6f554393c4e9e340c90e
David <davidmoussa@outlook.com>
2020-05-02 21:53:01 +0300

    Added Entity Highlighter

--- gamemodes/Jailbreak/gamemode/util/sh_entity_highlighter.lua
+++ gamemodes/Jailbreak/gamemode/util/sh_entity_highlighter.lua
@@ -1,5 +1,6 @@
 if SERVER then
     util.AddNetworkString("SendHighlights")
+    util.AddNetworkString("SentHighlightedPlayers")
 
     function JB:HighlightPlayer(players, targets)
         net.Start("SendHighlights")
@@ -18,10 +19,19 @@ if SERVER then
             net.Broadcast()
         end
     end
+
+    function JB:HighlightPlayers(players, tableName)
+        if not players then return end
+        net.Start("SentHighlightedPlayers")
+        net.WriteString(tableName and tableName or "undefined")
+        net.WriteTable(players)
+        net.Broadcast()
+    end
 end
 
 if CLIENT then
     local entList = {}
+    local highlightPlayers = {}
 
     hook.Add("PreDrawHalos", "HighlightPlayers", function()
         for k, v in pairs(entList) do
@@ -29,9 +39,23 @@ if CLIENT then
                 halo.Add({v}, team.GetColor(v:Team()), 1, 1, 2, true, true)
             end
         end
+
+        for k, v in pairs(highlightPlayers) do
+            for i, j in pairs(v) do
+                if j.Player:Alive() then
+                    halo.Add({j.Player}, j.Color,  1, 1, 2, true, true)
+                end
+            end
+        end
     end)
 
     net.Receive("SendHighlights", function()
         entList = net.ReadTable()
+    end)
+
+    net.Receive("SentHighlightedPlayers", function() 
+        local tableName = net.ReadString()
+        local players  = net.ReadTable()
+        highlightPlayers[tableName] = players
     end)
 end
