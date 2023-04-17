781bab084a07070352cbcca71c8f9b65b2bd53d5
Nullrefz <davidmoussa@outloo.com>
2023-04-17 23:26:34 +0300

    Fixed Log Reading / Writing

--- gamemodes/jailbreak/gamemode/logs/sv_logs.lua
+++ gamemodes/jailbreak/gamemode/logs/sv_logs.lua
@@ -14,12 +14,14 @@ local roundLogs = {}
 local session = 0
 local roundNumber = 0
 local dir = ""
-
 function JB:RegisterLog(instigator, entry)
-    if self:GetActivePhase() ~= ROUND_ACTIVE then return end
+    if self:GetActivePhase() ~= ROUND_ACTIVE then
+        return
+    end
     for k, v in pairs(roundLogs) do
         if v.User == instigator then
-            if entry.Type == "Death" then v.UserLifeSpan = entry.Time 
+            if entry.Type == "Death" then
+                v.UserLifeSpan = entry.Time
             end
             table.insert(v.Logs, entry)
             self:SaveLogs()
@@ -31,26 +33,37 @@ end
 util.AddNetworkString("SendLog")
 util.AddNetworkString("LogRequest")
 
-function JB:SendLog(ply)
-    if not ply then return end
+function JB:SendLog(ply, round)
+    if not ply then
+        return
+    end
+    local logs = self:LoadLogs(round)
     net.Start("SendLog")
-    net.WriteInt(roundNumber, 32)
-    net.WriteFloat(self:GetTimeElapsed())
-    net.WriteTable(roundLogs)
+    net.WriteInt(round, 32)
+    net.WriteFloat(logs.RoundTime)
+    net.WriteTable(logs)
     net.Send(ply)
 end
 
-function JB:HandleLogRequest(ply)
-    --TODO: This is the section where I check if the player is allowed to get the logs
-    self:SendLog(ply)
+function JB:HandleLogRequest(ply, round)
+    -- TODO: This is the section where I check if the player is allowed to get the logs
+    round = math.Clamp(round, 1, roundNumber)
+    self:SaveLogs()
+    self:SendLog(ply, round)
 end
 
 net.Receive("LogRequest", function(ln, ply)
-    JB:HandleLogRequest(ply)
+    local round = net.ReadInt(32)
+    if round == -1 then 
+        round = roundNumber
+    end
+    JB:HandleLogRequest(ply, round)
 end)
 
 function JB:SaveLogs()
-    if roundLogs == {} then return end
+    if roundLogs == {} then
+        return
+    end
     local json = util.TableToJSON(roundLogs)
 
     if not file.IsDir(dir, "DATA") then
@@ -60,20 +73,18 @@ function JB:SaveLogs()
     file.Write(dir .. "/round_" .. roundNumber .. ".txt", json)
 end
 
-function JB:LoadLogs(sessionID, round)
+function JB:LoadLogs(round)
     logs = {}
-    files = file.Find("*", dir, "nameasc")
-
-    for k, v in pairs(files) do
-        table.insert(logs, util.JSONToTable(v))
-    end
+    
+    logFile = file.Read(dir .."/round_" .. round .. ".txt", "DATA")
+    return util.JSONToTable(logFile)
 end
 
 function JB:SetupLogs()
     roundLogs = {}
     roundNumber = roundNumber + 1
 
-    for k, v in pairs (player.GetAll()) do 
+    for k, v in pairs(player.GetAll()) do
         table.insert(roundLogs, {
             User = v:IsBot() and v:Name() or v:SteamID(),
             UserTeam = v:Team(),
@@ -83,6 +94,7 @@ function JB:SetupLogs()
         })
     end
     
+    roundLogs.RoundTime = 0
     self:SaveLogs()
 
 end
@@ -95,11 +107,11 @@ hook.Add("jb_round_ending", "SaveLogs", function()
     JB:SaveLogs()
 end)
 
-hook.Add("Initialize", "SetupLogsFolder", function()
-    while (file.IsDir("jailbreak/logs/session_" .. session, "DATA")) do
-        session = session + 1
-    end
+hook.Add("Think", "StoreTime", function()
+    if not roundLogs.RoundTime then return end
+    roundLogs.RoundTime = roundLogs.RoundTime + FrameTime()
+end)
 
-    session = session + 1
-    dir = "jailbreak/logs/session_" .. session
+hook.Add("Initialize", "SetupLogsFolder", function()
+    dir = "jailbreak/logs/session_" .. os.time()
 end)
